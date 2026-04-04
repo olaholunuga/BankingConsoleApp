a02e276f1912613193a5d9c580a23b5fd5b7d8af
olaholunuga <olaholunuga@gmail.com>
2026-04-04 14:46:29 +0100

    created models with their respective constructors

---
+++ Models.cs
@@ -0,0 +1,57 @@
+public class User
+{
+    private Guid _id;
+    private string first;
+    private string last;
+    private Account account;
+
+    public User (string lastName, string firstName, Bank bank_name)
+    {
+        first = firstName;
+        last = lastName;
+        _id = Guid.NewGuid();
+        account = new Account(bank_name);
+    }
+
+}
+
+public class Account
+{
+    private long id;
+    private Bank bank;
+    private static int[] prefix_list = [207, 142, 657];
+
+    public Account (Bank bank_name)
+    {
+        int acc_prefix = prefix_list[Random.Shared.Next(prefix_list.Length)];
+        long acc_suffix = Random.Shared.Next(0, 10000000);
+
+        id = (acc_prefix * 10000000) + acc_suffix;
+        bank = bank_name;
+    }
+}
+
+public class Transaction
+{
+    private string _type; // outbound or inbound
+    private Guid id;
+    private long _amount; // 89700393456
+
+    public Transaction(string type, long amount)
+    {
+        id = Guid.NewGuid();
+        _type = type;
+        _amount = amount;
+    }
+}
+
+public class Bank
+{
+    private string _name;
+
+    public Bank(string name)
+    {
+        _name = name;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Banking Console App
+
+### Features
+- Users
+- Accounts
+- Other Banks
+- Other bank accounts
+- Users save
+- Users Withdraw
+- User Account Balance
+- User transfer
+- User Account Creation
